35e080479e2655f703ba43598a0b08cba5ded88b
Tuna205 <32743609+Tuna205@users.noreply.github.com>
2017-10-31 22:27:38 +0100

    Add files via upload

---
+++ Zadatak6i7/AsyncAwait.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Zadatak6i7
+{
+	public class AsyncAwait
+	{
+		private static async Task LetsSayUserClickedAButtonOnGuiMethod()
+		{
+			var result = await GetTheMagicNumber();
+			Console.WriteLine(result);
+		}
+		private static async Task<int> GetTheMagicNumber()
+		{
+			return await IKnowIGuyWhoKnowsAGuy();
+		}
+		private static async Task<int> IKnowIGuyWhoKnowsAGuy()
+		{
+			int rez1 = await IKnowWhoKnowsThis(10);
+			int rez2 = await IKnowWhoKnowsThis(5);
+			return rez1 + rez2;
+		}
+		private static async Task<int> IKnowWhoKnowsThis(int n)
+		{
+			int rez = await MagicNumber.FactorialDigitSum(n);
+			return rez;
+		}
+		// Ignore this part .
+		public static void Main(string[] args)
+		{
+			// Main method is the only method that
+			// can ’t be marked with async .
+			// What we are doing here is just a way for us to simulate
+			// async - friendly environment you usually have with
+			// other . NET application types ( like web apps , win apps etc .)
+			// Ignore main method , you can just focus on
+			// LetsSayUserClickedAButtonOnGuiMethod() as a
+			// first method in the call hierarchy .
+			var t = Task.Run(() => LetsSayUserClickedAButtonOnGuiMethod());
+			Console.Read();
+		}
+	}
+}
---
+++ Zadatak6i7/MagicNumber.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+
+namespace Zadatak6i7
+{
+    public class MagicNumber
+    {
+	    public static async Task<int> FactorialDigitSum(int n)
+	    {
+		    int faktorijel = await Factorial(n);
+		    int rez = await DigitSum(faktorijel);
+
+		    return rez;
+	    }
+
+	   
+
+	    private static async Task<int> Factorial(int n)
+	    {
+		    int rez = 1;
+		    for (int i = 2; i <= n; i++)
+		    {
+			    rez *= i;
+		    }
+		    return rez;
+	    }
+
+	    private static async Task<int> DigitSum(int faktorijel)
+	    {
+		    int rez = 0;
+		    while (faktorijel > 0)
+		    {
+			    int temp = faktorijel % 10;
+			    faktorijel /= 10;
+			    rez += temp;
+		    }
+
+		    return rez;
+	    }
+	}
+}
