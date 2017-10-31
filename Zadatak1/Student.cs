20baf1b7cfd6a2eb452ac88c7fac0892f77e514a
Tuna205 <32743609+Tuna205@users.noreply.github.com>
2017-10-31 22:19:51 +0100

    Add files via upload

---
+++ Zadatak1/Student.cs
@@ -0,0 +1,40 @@
+﻿namespace Zadatak1
+{
+	public class Student
+	{
+		public string Name { get; set; }
+		public string Jmbag { get; set; }
+		public Gender Gender { get; set; }
+		public Student(string name, string jmbag)
+		{
+			Name = name;
+			Jmbag = jmbag;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Student objAStudent)) return false;
+			return Jmbag.Equals(objAStudent.Jmbag);
+		}
+
+		public override int GetHashCode()
+		{
+			return Jmbag.GetHashCode();
+		}
+
+		public static bool operator ==(Student std1, Student std2)
+		{
+			return std1.Jmbag.Equals(std2.Jmbag);
+		}
+
+		public static bool operator !=(Student std1, Student std2)
+		{
+			if (std1 == null) return true;
+			return !(std1 == std2);
+		}
+	}
+	public enum Gender
+	{
+		Male, Female
+	}	
+}
---
+++ Zadatak1/Test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadatak1
+{
+	public class Test
+	{
+		public static void Main(String[] args)
+		{
+			Case1();
+			Case2();
+			Case3();
+		}
+
+
+		static void Case1()
+		{
+			var topStudents = new List<Student>()
+			{
+				new Student(" Ivan ", jmbag: " 001234567 "),
+				new Student(" Luka ", jmbag: " 3274272 "),
+				new Student("Ana", jmbag: " 9382832 ")
+			};
+			var ivan = new Student(" Ivan ", jmbag: " 001234567 ");
+			// false :(
+			bool isIvanTopStudent = topStudents.Contains(ivan); //contains nevalja
+			Console.WriteLine("Case1: " + isIvanTopStudent );
+		}
+
+		static void Case2()
+		{
+			var list = new List<Student>()
+			{
+				new Student(" Ivan ", jmbag: " 001234567 "),
+				new Student(" Ivan ", jmbag: " 001234567 ")
+			};
+			// 2 :(
+			var distinctStudentsCount = list.Distinct().Count(); // printa ih duplo makar je distinct
+			Console.WriteLine("Case2: " + distinctStudentsCount);
+		}
+
+		static void Case3()
+		{
+			var topStudents = new List<Student>()
+			{
+				new Student(" Ivan ", jmbag: " 001234567 "),
+				new Student(" Luka ", jmbag: " 3274272 "),
+				new Student("Ana", jmbag: " 9382832 ")
+			};
+			var ivan = new Student(" Ivan ", jmbag: " 001234567 ");
+			// false :(
+			// == operator is a different operation from . Equals ()
+			// Maybe it isn ’t such a bad idea to override it as well
+			bool isIvanTopStudent = topStudents.Any(s => s == ivan); //promijeniti == u equals
+			Console.WriteLine("Case3: " + isIvanTopStudent);
+		}
+	}
+}
